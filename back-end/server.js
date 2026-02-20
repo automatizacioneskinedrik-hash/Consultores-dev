cfdbc306e734736cdb92a7ea191fc74fe42b36fe
brayanportillaeadic-cmyk <brayanportilla.eadic@gmail.com>
2026-02-20 15:36:31 -0500

    cloudbuild.yaml

---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const port = Number(process.env.PORT) || 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/healthz') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+    return;
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    service: 'kinedrik-backend',
+    status: 'running'
+  }));
+});
+
+server.listen(port, () => {
+  console.log(`Backend listening on port ${port}`);
+});
